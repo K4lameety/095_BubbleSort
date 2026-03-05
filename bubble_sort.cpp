13b778c47e647b960b0c99d9a587c18dde9741d8
K4lameety <rhkapota@gmail.com>
2026-03-05 18:34:58 +0700

    add bubblesort procedur

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -35,6 +35,34 @@ void input()
     }
 }
 
+/** BubbleSort Procedur */
+void bubbleSortArray()
+{
+    int pass = 1;
+
+    do 
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass += 1;
+
+        cout << "\nPass " << pass - 1 << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+        cout << endl;
+    } while (pass <= n - 1);
+}
+
 /** Main Program */
 int main(){
     input();
